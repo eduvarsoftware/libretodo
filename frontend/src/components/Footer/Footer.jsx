9dac0a12f0e7b2e045c86f2ba1475bdcd269fe96
eduvarsoftware <eduvarsoftware@outlook.com>
2023-02-21 02:53:44 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,3 +1,7 @@
-const Footer = () => (<footer className="footer">(c) Eduvar Software Pvt Ltd, 2023.</footer>)
+const Footer = () => (
+  <footer className="footer">
+    <div>(c) Eduvar Software Pvt Ltd, 2023. LibreTODO is open source under the <a href="https://raw.githubusercontent.com/eduvarsoftware/libretodo/main/LICENSE">3-clause BSD license</a>. Follow LibreTODO on <a href="https://github.com/eduvarsoftware/libretodo">GitHub</a>.</div>
+  </footer>
+);
 
 export default Footer;
